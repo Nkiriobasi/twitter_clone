4fb0d3b278d9bff6dd2a9d0498c211527211d595
Nkiriobasi <obasinkiri@gmail.com>
2023-04-04 18:23:50 +0100

    added protected routes to account page

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NavigationBar from './components/NavigationBar';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Account from "./pages/Account";
+
+
+
+function App() {
+  return (
+    <main className='max-w-[1000px] mx-auto md:px-0 px-5'>
+      <AuthContextProvider>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </main>
+  );
+}
+
+export default App;
